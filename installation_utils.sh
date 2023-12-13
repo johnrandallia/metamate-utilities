734cb8406b6d0688a53fef6c4f6888246a10d016
johnrandallia <153721406+johnrandallia@users.noreply.github.com>
2023-12-13 16:06:09 +0000

    Update installation_utils.sh

--- installation_utils.sh
+++ installation_utils.sh
@@ -5,6 +5,6 @@
 conda install -c bioconda fastqc
 conda install -c bioconda fastx_toolkit
 conda install -c bioconda trimmomatic
-conda install -c bioconda samtools
+conda install -c "bioconda/label/cf201901" samtools
 conda install -c bioconda vsearch
 
