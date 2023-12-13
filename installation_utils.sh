6b71f3ed570e2933e25c5f1532e00d31bec568d1
johnrandallia <153721406+johnrandallia@users.noreply.github.com>
2023-12-13 10:28:13 +0000

    Create installation_utils.sh

---
+++ installation_utils.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Installation of Utilities
+
+conda install -c bioconda fastqc
+conda install -c bioconda fastx_toolkit
+conda install -c bioconda trimmomatic
+conda install -c bioconda samtools
+conda install -c bioconda vsearch
+
